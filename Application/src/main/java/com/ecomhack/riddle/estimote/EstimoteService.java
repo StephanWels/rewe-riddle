916b4e7af6c690e95a92b7d42a7bd3f4ea3fafb8
Stephan Wels <stephan.wels@rewe-digital.com>
2015-05-09 15:39:00 +0200

    added estimate service

--- Application/src/main/java/com/ecomhack/riddle/estimote/EstimoteService.java
+++ Application/src/main/java/com/ecomhack/riddle/estimote/EstimoteService.java
@@ -1,29 +1,29 @@
 package com.ecomhack.riddle.estimote;
 
-import android.bluetooth.BluetoothClass;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
-public class EstimoteService extends BluetoothClass.Service {
- @Override
- public IBinder onBind(Intent arg0) {
-  return null;
- }
- 
- @Override
- public int onStartCommand(Intent intent, int flags, int startId) {
-  try {
-   <b>EstimoteManager.Create((NotificationManager) this
-     .getSystemService(Context.NOTIFICATION_SERVICE), this,
-     intent);</b>
-  } catch (Exception e) {
-  }
-  return START_STICKY;
- }
- 
- @Override
- public void onDestroy() {
-  super.onDestroy();
-  <b>EstimoteManager.stop();</b>
- }
+public class EstimoteService extends Service {
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try {
+            EstimoteManager.Create((NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE), this, intent);
+        } catch (Exception e) {
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EstimoteManager.stop();
+    }
 }
