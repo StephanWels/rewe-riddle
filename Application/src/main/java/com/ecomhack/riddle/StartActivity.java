dcb987aec9f826fab52a60c1a7672c1e542e10ae
Stephan Wels <stephan.wels@rewe-digital.com>
2015-05-09 22:00:59 +0200

    Removed some static stuff. Now activating BT on startup Cleanup Near products cache on startup

--- Application/src/main/java/com/ecomhack/riddle/StartActivity.java
+++ Application/src/main/java/com/ecomhack/riddle/StartActivity.java
@@ -1,11 +1,14 @@
 package com.ecomhack.riddle;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.ecomhack.riddle.estimote.EstimoteReceiver;
+import com.ecomhack.riddle.estimote.EstimoteService;
 import com.example.android.basicnotifications.R;
 
 /**
@@ -15,13 +18,35 @@ public class StartActivity extends Activity {
 
         private static final String TAG = "start";
 
-        @Override
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                // Bluetooth is not enable
+                Intent btIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                btIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getApplicationContext().startActivity(btIntent);
+            } else {
+                // Bluetooth is already enabled
+                getApplicationContext().startService(new Intent(getApplicationContext(),
+                        EstimoteService.class));
+            }
+        }
+
+    }
+
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.start);
         }
         public void startBreakfastChallenge(View view){
-            Log.i("riddle","start breakfast");
+            Log.i("riddle", "start breakfast");
             Intent intent = new Intent(this, RiddleActivity.class);
             startActivity(intent);
         }
