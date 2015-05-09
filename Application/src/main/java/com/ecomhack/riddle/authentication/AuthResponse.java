5a7d4b94b783fda9c46c08e7b226baa07eb77e62
Ansgar Brauner <ansgar.brauner@rewe-digital.com>
2015-05-09 16:54:01 +0200

    Start working on Basic Auth Stuff

---
+++ Application/src/main/java/com/ecomhack/riddle/authentication/AuthResponse.java
@@ -0,0 +1,43 @@
+package com.ecomhack.riddle.authentication;
+
+public class AuthResponse {
+
+    private String accessToken;
+    private String tokenType;
+    private String expiresIn;
+    private String scope;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(String expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+}
+
+
--- Application/src/main/java/com/ecomhack/riddle/sphere/SphereTask.java
+++ Application/src/main/java/com/ecomhack/riddle/sphere/SphereTask.java
@@ -2,22 +2,34 @@ package com.ecomhack.riddle.sphere;
 
 
 import android.os.AsyncTask;
+import android.util.Base64;
 import android.util.Log;
 
+import com.ecomhack.riddle.Authentication.AuthResponse;
+import com.ecomhack.riddle.authentication.AuthResponse;
+
+import org.springframework.http.HttpEntity;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URL;
 
 public class SphereTask extends AsyncTask<URL, Integer, Long> {
 
+    private static final String SPHERE_USER = "il1AWMhTtKeBoTsldA-r31GX";
+    private static final String SPHERE_PASSWORD = "96bG47J4diYfudRj0JBf52qZopBswBcv";
+    private static final String SPHERE_USER_PASSWORD = SPHERE_USER + ":" + SPHERE_PASSWORD;
+    private static final String SPHERE_AUTH_SCOPE = "grant_type=client_credentials&scope=manage_project:rewe-riddle-2";
+
     private static final String TAG = "SphereActivity";
 
     protected Long doInBackground(URL... urls) {
 
             // The connection URL
-            String url = "https://ajax.googleapis.com/ajax/" +
-                    "services/search/web?v=1.0&q={query}";
+            String url = "https://auth.sphere.io/oauth/token";
 
 // Create a new RestTemplate instance
             RestTemplate restTemplate = new RestTemplate();
@@ -26,7 +38,15 @@ public class SphereTask extends AsyncTask<URL, Integer, Long> {
             restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
 
 // Make the HTTP GET request, marshaling the response to a String
-            String result = restTemplate.getForObject(url, String.class, "Android");
+        final String basicAuth = "Basic " + Base64.encodeToString(SPHERE_USER_PASSWORD.getBytes(), Base64.NO_WRAP);
+
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+        headers.add("Authorization", basicAuth);
+
+        HttpEntity<String> request = new HttpEntity<String>(SPHERE_AUTH_SCOPE, headers);
+
+        AuthResponse result = restTemplate.postForObject(url, request, AuthResponse.class);
+
 
         Log.v(TAG, "testing" + result);
 
